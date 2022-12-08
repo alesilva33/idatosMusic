c7cd41284e7060c530c5694e6c3966ce80988ca5
Alejandro Silva <asilvavidal97@gmail.com>
2022-12-08 19:30:57 -0300

    Data integration 1.0 completed

--- dataIntegration.py
+++ dataIntegration.py
@@ -1,18 +1,15 @@
 import csv
 from difflib import SequenceMatcher
-import numpy as np
-
 from enum import Enum
 
 DATASETS_FILEPATH = 'Datasets'
 MINIMUM_MATCH_PERCENTAGE = 0.8
+NUMBER_OF_ITERATIONS = 2000
 
 class Entries(Enum):
     NAME = 1
     ALBUM = 2
-    ALBUM_ID = 3
     ARTIST = 4
-    ARTIST_ID = 5
     DANCEABILITY = 9
     ENERGY = 10
     KEY = 11
@@ -23,99 +20,103 @@ class Entries(Enum):
     DURATION = 20
     YEAR = 22
 
-"""id,name1,album2,album_id3,artists4,artist_ids5,
-track_number6,disc_number7,explicit8,danceability9,
-energy10,key11,loudness12,mode13,speechiness14,acousticness15,
-instrumentalness16,liveness17,valence18,tempo19,duration_ms20,
-time_signature21,year22,release_date23"""
-
 songs = {}
-songsIds = []
-songsNames = []
-songsAlbums = []
-albumsIds = []
-artists = []
-artistsIds = []
-trackNumbers = []
-disc_number = []
-danceability = []
-energy = []
-key = []
-loduness = []
-acousticness = []
-tempos = []
-durations = []
-years = []
 
 f = open('integratedData.csv', 'w')
 writer = csv.writer(f)
-with open(DATASETS_FILEPATH + '/tracks_features.csv') as csv_file:
+
+# guitarTabs header: Artist,Song Name,Song Rating,Song Hits,Page Type,Difficulty,Key,Capo,Tuning
+with open(DATASETS_FILEPATH + '/gutiarTabs.csv') as csv_file:
     csv_reader = csv.reader(csv_file)
     first = True
     songId = 0
     for row in csv_reader:
         if not first:
-            songName = row[Entries.NAME.value].strip()
+            songName = row[1].strip()
             songs[songName] = []
             songs[songName].append(songId)
             songs[songName].append(songName)
-            songs[songName].append(row[Entries.ALBUM.value].strip())
-            songs[songName].append(row[Entries.ALBUM_ID.value])
-            songs[songName].append(row[Entries.ARTIST.value][0])
-            songs[songName].append(row[Entries.ARTIST_ID.value][0])
-            songs[songName].append(row[Entries.DANCEABILITY.value])
-            songs[songName].append(row[Entries.ENERGY.value])
-            songs[songName].append(row[Entries.KEY.value])
-            songs[songName].append(row[Entries.ACOUSTICNESS.value])
-            songs[songName].append(row[Entries.TEMPO.value])
-            songs[songName].append(row[Entries.DURATION.value])
-            songs[songName].append(row[Entries.YEAR.value])
-            #writer.writerow(songs[songName])
-            """ songs[songId] = []
-            songs[songId].append(songId)
-            songs[songId].append(row[Entries.NAME.value].strip())
-            songs[songId].append(row[Entries.ALBUM.value].strip())
-            songs[songId].append(row[Entries.ALBUM_ID.value])
-            songs[songId].append(row[Entries.ARTIST.value][0])
-            songs[songId].append(row[Entries.ARTIST_ID.value][0])
-            songs[songId].append(row[Entries.DANCEABILITY.value])
-            songs[songId].append(row[Entries.ENERGY.value])
-            songs[songId].append(row[Entries.KEY.value])
-            songs[songId].append(row[Entries.ACOUSTICNESS.value])
-            songs[songId].append(row[Entries.TEMPO.value])
-            songs[songId].append(row[Entries.DURATION.value])
-            songs[songId].append(row[Entries.YEAR.value])
-            writer.writerow(songs[songId]) """
+            songs[songName].append(row[0])
+            songs[songName].append(row[5])
+            songs[songName].append(row[6])
+            songs[songName].append(row[7])
+            songs[songName].append(row[8])
             songId += 1
         else:
             first = False
 
+"""id,name,album,album_id,artists,
+artist_ids,track_number,disc_number,
+explicit,danceability,energy,key,loudness,
+mode,speechiness,acousticness,instrumentalness,
+liveness,valence,tempo,duration_ms,time_signature,
+year,release_date"""
+
+with open(DATASETS_FILEPATH + '/tracks_features.csv') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    first = True
+    rows = list(csv_reader)
+    for song in songs:
+        iterator = 1
+        songFound = False
+        while not songFound and iterator < NUMBER_OF_ITERATIONS:
+            row = rows[iterator]
+            songMatchPercentage = SequenceMatcher(a=song, b=row[1]).ratio()
+            if songMatchPercentage > MINIMUM_MATCH_PERCENTAGE:
+                songsArtistName = songs[song][2]
+                csvArtistName = row[4].split("'")[1]
+                songArtistMatchPercentage = SequenceMatcher(a=songsArtistName, b=csvArtistName).ratio()
+                if songArtistMatchPercentage > MINIMUM_MATCH_PERCENTAGE:
+                    songs[song].append(row[Entries.ALBUM.value].strip())
+                    songs[song].append(row[Entries.DANCEABILITY.value])
+                    songs[song].append(row[Entries.ENERGY.value])
+                    songs[song].append(row[Entries.KEY.value])
+                    songs[song].append(row[Entries.ACOUSTICNESS.value])
+                    songs[song].append(row[Entries.INSTRUMENTALNESS.value])
+                    songs[song].append(row[Entries.VELANCE.value])
+                    songs[song].append(row[Entries.TEMPO.value])
+                    songs[song].append(row[Entries.DURATION.value])
+                    songs[song].append(row[Entries.YEAR.value])
+                    songs[song].append("No Lyrics")
+                    songFound = True
+            iterator += 1
+        if iterator >= NUMBER_OF_ITERATIONS:
+            songs[song].append("No Album info")
+            songs[song].append(0.0)
+            songs[song].append(0.0)
+            songs[song].append(0.0)
+            songs[song].append(0.0)
+            songs[song].append(0.0)
+            songs[song].append(0.0)
+            songs[song].append(0.0)
+            songs[song].append("No Duration info")
+            songs[song].append("No Year info")
+            songs[song].append("No Lyrics info")
+
+# lyrics header: id,artist,seq,song,label
 with open(DATASETS_FILEPATH + '/lyrics.csv') as csv_file:
     csv_reader = csv.reader(csv_file)
     first = True
     songId = 0
-    for row in csv_reader:
-        if not first:
-            # Formato row: id,artist,seq,song,label
-            songArtist = row[1]
-            songLyrics = row[2]
-            songName = row[3]
-            songFound = False
-            songIterator = 0
-            while not songFound:
-                songsSongName = songs.keys()[songIterator]
-                songMatchPercentage = SequenceMatcher(songsSongName=songsSongName, songName=songName).ratio()
-                if songMatchPercentage > MINIMUM_MATCH_PERCENTAGE:
-                    songsArtistName = songs[songsSongName][5]
-                    songArtistMatchPercentage = SequenceMatcher(songsArtistName=songsArtistName, songArtist=songArtist).ratio()
-                    if songArtistMatchPercentage > MINIMUM_MATCH_PERCENTAGE:
-                        songs[songsSongName].append(songLyrics)
-        else:
-            first = False
-f.close()
+    rows = list(csv_reader)
+    for song in songs:
+        iterator = 1
+        songFound = False
+        while not songFound and iterator < NUMBER_OF_ITERATIONS:
+            row = rows[iterator]
+            songMatchPercentage = SequenceMatcher(a=song, b=row[3]).ratio()
+            if songMatchPercentage > MINIMUM_MATCH_PERCENTAGE:
+                songsArtistName = songs[song][2]
+                csvArtistName = row[1]
+                songArtistMatchPercentage = SequenceMatcher(a=songsArtistName, b=csvArtistName).ratio()
+                if songArtistMatchPercentage > MINIMUM_MATCH_PERCENTAGE:
+                    songs[song][17] = row[2]
+                    songFound = True
+            iterator += 1
 
-# open the file in the write mode
-"""f = open('integratedData.csv', 'w')
-writer = csv.writer(f)
-writer.writerow(songs)
-f.close()"""
+# Final format: 
+# id, songName, artistName, difficulty, tone, capo, tuning, album, danceability, energy, key, acousticness, intrumentalness, velance, tempo, duration, year, lyrics
+for song in songs:
+    writer.writerow(songs[song])
+
+f.close()
